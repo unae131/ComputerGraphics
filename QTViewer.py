bb8b492946e326c37195959eee06ba9f1a5913ac
unae131 <yjhyhw8998@gmail.com>
2021-04-15 02:43:20 +0900

    renewed UI
    Mouse handlers were added!

--- QTViewer.py
+++ QTViewer.py
@@ -1,129 +1,51 @@
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
- 
-import sys
 from GLDrawer import *
 from BvhParser import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QOpenGLWidget
+from baseUI import *
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QOpenGLWidget
 import platform
  
- 
-class QTViewer(QOpenGLWidget):
-    RED = [1.0, 0.0, 0.0]
-    BLUE = [0.0, 0.0, 1.0]
- 
-    flag = 1
- 
-    def  __init__(self, fileName, parent = None):
-        super(QTViewer, self).__init__(parent)
+class QtViewer(QOpenGLWidget):
+    def  __init__(self, fileName, label, slider, parent = None):
+        super(QtViewer, self).__init__(parent)
         self.glDrawer = GLDrawer(fileName)
+        self.label = label
+        self.slider = slider
+        self.mousePressed = False
+        self.panning = False
         self.initUI()
 
     def initUI(self):
-        cfrm = QFrame(self)
-        # self.setCentralWidget(cfrm)
-        grid = QGridLayout()
-        cfrm.setLayout(grid)
-
-        self.drawBtn("프레임 on/off", 20, 650, self.frameOnOff)
-        self.playOrStopBtn = self.drawBtn("재생/멈춤", 20, 750, self.glDrawer.switchPlaying)
-        self.frameMoveBtn = self.drawBtn("프레임 이동", 120, 750, self.frameMove)
-        self.zoomInBtn = self.drawBtn("+", 20, 700, self.glDrawer.zoomIn)
-        self.zoomOutBtn = self.drawBtn("-", 50, 700, self.glDrawer.zoomOut)
-
-        self.code_edit = QLineEdit(self)
-        self.code_edit.move(220, 755)
-        self.code_edit.setText("")
-
-        self.label = QLabel('Frame: '+str(self.glDrawer.curFrame)+"/" + str(self.glDrawer.motion.frames), self)
-        self.label.move(350, 755)
-        self.label.setStyleSheet("Color : white")
-        self.label.resize(self.label.sizeHint())
-        self.label.setGeometry(100,100,400,100)
-
-        self.sld = QSlider(Qt.Horizontal, cfrm)
-        self.sld.adjustSize()
-        self.sld.setTickPosition(1)
-        self.sld.setMaximum(self.glDrawer.motion.frames - 1)
-        self.sld.setMinimum(0)
-        self.sld.valueChanged[int].connect(self.slideFrame)
-        grid.addWidget(self.sld, 20, 10, 20, 80)
-
-        #progress bar
-        self.pbar = QProgressBar(cfrm)
-        self.pbar.adjustSize()
-        self.pbar.text()
-        grid.addWidget(self.pbar, 20,0,10,80)
-        self.timer = QTimer()
-        self.step = 1#self.glDrawer.curFrame
-
-    def drawBtn(self, name, x, y, btn_callback):
-        btn = QPushButton(name, self)
-        btn.move(x,y)
-        # print(self.playOrStopbtn.sizeHint())
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(btn_callback)
-        # btn.clicked.connect(QCoreApplication.instance().quit)
-        self.setGeometry(100,100,200,100)
-
-        return btn
-
-    def frameMove(self):
-        frame = self.code_edit.text()
-        self.glDrawer.setFrame(int(frame))
-        self.label.setText('Frame: '+str(self.glDrawer.curFrame)+"/" + str(self.glDrawer.motion.frames))
-    
-    def slideFrame(self):
-        frame = self.sld.value()
-        self.glDrawer.setFrame(frame)
-        self.label.setText('Frame: '+str(self.glDrawer.curFrame)+"/" + str(self.glDrawer.motion.frames))
-    
-    def progressBar(self):
-        self.glDrawer.switchPlaying()
-        self.sld.setValue(self.glDrawer.curFrame)
-        self.pbar.setValue(self.glDrawer.curFrame)
-
-    def frameOnOff(self):
-        self.glDrawer.fill = not self.glDrawer.fill
+        self.setMouseTracking(True)
 
     def paintGL(self):
- 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()
- 
-        if QTViewer.flag:
-            glColor3f(QTViewer.RED[0], QTViewer.RED[1], QTViewer.RED[2])
-        else :
-            glColor3f(QTViewer.BLUE[0], QTViewer.BLUE[1], QTViewer.BLUE[2])
- 
-        # glPushMatrix()
-        # glTranslatef(0.0, 0.0, -5.0)
-        # glRotatef(60.0, 1.0, 1.0, 0.0)
         self.glDrawer.render()
-        self.label.setText('Frame: '+str(self.glDrawer.curFrame)+"/" + str(self.glDrawer.motion.frames-1))
-        # self.sld.setValue(self.glDrawer.curFrame)
-        # self.pbar.setValue(self.glDrawer.curFrame)
-        # # self.Draw(1.0, 1.0, 1.0)
-        # glPopMatrix()
-        glFlush()
-        
+        self.label.setText(str(self.glDrawer.curFrame)+"/" + str(self.glDrawer.motion.frames-1))
+        self.slider.setValue(self.glDrawer.curFrame)
         self.update()
  
     def mousePressEvent(self, event):
-        if QTViewer.flag :
-            QTViewer.flag = 0
-        else :
-            QTViewer.flag = 1
-        
- 
-if __name__=='__main__':
-    app = QApplication(sys.argv)
-    window = QTViewer("bvhFiles/sample-walk.bvh")
-    window.setWindowTitle('QTViewer')
-    window.setFixedSize(1200,800)
-    window.show()
-    sys.exit(app.exec_())
+        self.mousePressed = True
+        self.x = event.x()
+        self.y = event.y()
+
+    def mouseReleaseEvent(self, event):
+        self.mousePressed = False
+
+    def mouseMoveEvent(self, e):
+        if self.mousePressed:
+            x_offset = 0.01*(e.x() - self.x)
+            y_offset = 0.05*(e.y() - self.y)
+
+            if not self.panning:
+                self.glDrawer.camera.orbit(x_offset, y_offset)
+
+            else:
+                self.glDrawer.camera.panning(x_offset, y_offset)
+
+            self.x = e.x()
+            self.y = e.y()
+
+    def mouseDoubleClickEvent(self, e):
+        self.panning = not self.panning
---
+++ baseUI.py
@@ -0,0 +1,116 @@
+from QtViewer import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow, resize):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(720, 490)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(640, 430, 61, 16))
+        self.label.setObjectName("label")
+
+        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider.setGeometry(QtCore.QRect(100, 430, 531, 22))
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+
+        self.openGLWidget = QtViewer("bvhFiles/sample-walk.bvh", label = self.label, slider = self.horizontalSlider, parent = self.centralwidget)
+        self.openGLWidget.setGeometry(QtCore.QRect(-1, -21, 721, 411))
+        self.openGLWidget.setObjectName("openGLWidget")
+        self.openGLWidget.show()
+
+        self.horizontalSlider.setRange(0,self.openGLWidget.glDrawer.motion.frames-1)
+        self.horizontalSlider.setPageStep(5)
+        self.horizontalSlider.valueChanged.connect(self.updateLabel)
+        
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(0, 430, 102, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.openGLWidget.glDrawer.switchPlaying)
+
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton.setGeometry(QtCore.QRect(210, 410, 100, 20))
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton.setChecked(False)
+        self.radioButton.clicked.connect(self.on_toggle_mode)
+
+        self.zoomInButton = QtWidgets.QPushButton(self.centralwidget)
+        self.zoomInButton.setGeometry(QtCore.QRect(640, 400, 33, 30))
+        self.zoomInButton.setObjectName("zoomInButton")
+        self.zoomInButton.clicked.connect(self.zoomIn_callback)
+
+        self.zoomOutButton = QtWidgets.QPushButton(self.centralwidget)
+        self.zoomOutButton.setGeometry(QtCore.QRect(670, 400, 33, 30))
+        self.zoomOutButton.setObjectName("zoomOutButton")
+        self.zoomOutButton.clicked.connect(self.zoomOut_callback)
+
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(10, 400, 193, 33))
+        self.widget.setObjectName("widget")
+
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        self.lineEdit = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.lineEdit.textChanged.connect(self.onChanged)
+
+        self.pushButton_3 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        self.pushButton_3.clicked.connect(self.onClicked)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        
+        # self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        # self.statusbar.setObjectName("statusbar")
+        # MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "시작/멈춤"))
+        self.label.setText(_translate("MainWindow", str(self.openGLWidget.glDrawer.curFrame)+"/"+str(self.openGLWidget.glDrawer.motion.frames)))
+        self.radioButton.setText(_translate("MainWindow", "Wire Frame"))
+        self.lineEdit.setText(_translate("MainWindow", "0"))
+        self.pushButton_3.setText(_translate("MainWindow", "이동"))
+        self.zoomInButton.setText(_translate("MainWindow", "+"))
+        self.zoomOutButton.setText(_translate("MainWindow", "-"))
+
+    def updateLabel(self, value):
+        self.openGLWidget.glDrawer.curFrame = value
+
+    def on_toggle_mode(self):
+        if self.radioButton.isChecked():
+            self.openGLWidget.glDrawer.fill = False
+        else:
+            self.openGLWidget.glDrawer.fill = True
+
+    def onChanged(self, frame):
+        self.lineEdit.setText(frame)
+
+    def onClicked(self, frame):
+        self.openGLWidget.glDrawer.curFrame = int(self.lineEdit.text())
+
+    def zoomIn_callback(self):
+        self.openGLWidget.glDrawer.camera.zoom(0.5)
+
+    def zoomOut_callback(self):
+        self.openGLWidget.glDrawer.camera.zoom(-0.5)    
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow, 2)
+    MainWindow.show()
+    sys.exit(app.exec_())
